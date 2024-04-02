a372564dd7729fdec7810f2be67784149c818761
André <andre.vrlcarvalho@hotmail.com>
2024-04-02 20:45:10 -0300

    ajustes

--- Salve a Terra - Alunos/js/jogo.js
+++ Salve a Terra - Alunos/js/jogo.js
@@ -1,8 +1,8 @@
 function loop(){
-    var posicao = parseInt(
-        $('#area_jogo').css('background-position')
-        )
-        $('#area_jogo').css('background-position', posicao - 1)
+    var position = parseInt($('#area_jogo').css('background-position'))
+
+    $('#area_jogo')
+        .css('background-position', position - 1)
 }
 
 setInterval(
